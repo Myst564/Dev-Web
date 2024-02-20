8a739dec0faea1c6ba1ebfccf6d3e08c71c86c33
Max Mayer <83821014+Myst564@users.noreply.github.com>
2024-02-20 10:51:25 +0100

    maj site web

--- HTML/JS Site web.js
+++ HTML/JS Site web.js
@@ -1,8 +1,7 @@
 // Fonction pour faire défiler jusqu'en haut de la page en douceur
 function scrollToTop() {
-    const c = document.documentElement.scrollTop || document.body.scrollTop;
-    if (c > 0) {
-        window.requestAnimationFrame(scrollToTop);
-        window.scrollTo(0,0)
-    }
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }

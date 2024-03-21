3538fd31a89fbd874ccebbd14abd59b55cbe97f7
Max Mayer <83821014+Myst564@users.noreply.github.com>
2024-03-21 11:15:39 +0100

    maj Jest ( test)

---
+++ HTML/Projet ECF Front End/__tests__/jest.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // Indique à Jest d'utiliser le preset pour les projets JavaScript
+    preset: 'ts-jest',
+    // Indique à Jest d'ignorer les fichiers dans les dossiers node_modules et build
+    testPathIgnorePatterns: [
+      '/node_modules/',
+      '/build/',
+    ],
+    // Définir les extensions de fichiers à tester
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    // Définir les dossiers à inclure dans les tests
+    roots: ['<rootDir>/src'],
+    // Indique à Jest d'utiliser babel-jest pour transpiler les fichiers JS/JSX
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    // Ignorer les fichiers de configuration de babel et typescript
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    // Utiliser la couverture de code pour évaluer la qualité des tests
+    collectCoverage: true,
+    // Définir les dossiers pour la couverture de code
+    coverageDirectory: 'coverage',
+  };
+  
